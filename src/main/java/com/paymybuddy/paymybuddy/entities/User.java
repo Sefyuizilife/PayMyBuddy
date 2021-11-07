29127c97678ada2a21a66c1c41be26f9fb69d65a
Sabouni Youssef <youssefsabouni@icloud.com>
2021-11-07 02:15:40 +0100

    feature(entities): create user entity

--- src/main/java/com/paymybuddy/paymybuddy/entities/User.java
+++ src/main/java/com/paymybuddy/paymybuddy/entities/User.java
@@ -1,26 +1,46 @@
 package com.paymybuddy.paymybuddy.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long   id;
-    private String lastname;
-    private String firstname;
-    private String email;
-    private String password;
-    private String IBAN;
-    private Double moneyAvailable;
+    private Long          id;
+    private String        lastName;
+    private String        firstName;
+    private String        email;
+    private String        password;
+    private String        IBAN;
+    private Double        moneyAvailable;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime updatedAt = LocalDateTime.now();
 
     @ManyToMany
-    private List<User> beneficiaries;
+    private List<User> contacts;
 
     public User() {
 
+    }
+
+    public User(String email, String password) {
+
+        this.email    = email;
+        this.password = password;
+    }
+
+    public String getFullName() {
+
+        return String.format("%s %s", this.lastName, this.firstName);
     }
 
     public Long getId() {
@@ -33,24 +53,24 @@ public class User {
         this.id = id;
     }
 
-    public String getLastname() {
+    public String getLastName() {
 
-        return lastname;
+        return lastName;
     }
 
-    public void setLastname(String lastname) {
+    public void setLastName(String lastname) {
 
-        this.lastname = lastname;
+        this.lastName = lastname;
     }
 
-    public String getFirstname() {
+    public String getFirstName() {
 
-        return firstname;
+        return firstName;
     }
 
-    public void setFirstname(String firstname) {
+    public void setFirstName(String firstname) {
 
-        this.firstname = firstname;
+        this.firstName = firstname;
     }
 
     public String getEmail() {
@@ -93,13 +113,73 @@ public class User {
         this.moneyAvailable = moneyAvailable;
     }
 
-    public List<User> getBeneficiaries() {
+    public List<User> getContacts() {
 
-        return beneficiaries;
+        return contacts;
     }
 
-    public void setBeneficiaries(List<User> beneficiaries) {
+    public void setContacts(List<User> contacts) {
 
-        this.beneficiaries = beneficiaries;
+        this.contacts = contacts;
+    }
+
+    public LocalDateTime getCreatedAt() {
+
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        authorityList.add(new SimpleGrantedAuthority(""));
+
+        return authorityList;
+    }
+
+
+    @Override
+    public String getUsername() {
+
+        return this.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+
+        return true;
     }
 }
