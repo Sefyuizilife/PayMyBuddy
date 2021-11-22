941d4afe5a9fa37a0e2e600626da01252dbe62a1
Sabouni Youssef <youssefsabouni@icloud.com>
2021-11-22 20:03:46 +0100

    feature(configurations): add security permission

---
+++ src/main/java/com/paymybuddy/paymybuddy/configurations/WebSecurityConfiguration.java
@@ -0,0 +1,49 @@
+package com.paymybuddy.paymybuddy.configurations;
+
+import com.paymybuddy.paymybuddy.services.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final UserService           userService;
+    private final BCryptPasswordEncoder encoder;
+
+    public WebSecurityConfiguration(UserService userService, BCryptPasswordEncoder encoder) {
+
+        this.userService = userService;
+        this.encoder     = encoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        http.authorizeRequests().antMatchers("/static/**", "/h2-console/**").permitAll();
+
+        http.authorizeRequests().anyRequest().authenticated();
+
+        http.formLogin().loginPage("/login").permitAll().defaultSuccessUrl("/transactions");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
+
+        authManagerBuilder.userDetailsService(userService).passwordEncoder(encoder);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+
+        return super.authenticationManagerBean();
+    }
+}
