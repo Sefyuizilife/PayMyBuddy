df269fc4fc701104c41a4e92f56d2339c8e7597a
Sabouni Youssef <youssefsabouni@icloud.com>
2021-11-23 07:00:00 +0100

    feature(PayMyBuddyApplication): Add bean for bcrypt encoder

--- src/main/java/com/paymybuddy/paymybuddy/PayMyBuddyApplication.java
+++ src/main/java/com/paymybuddy/paymybuddy/PayMyBuddyApplication.java
@@ -2,12 +2,20 @@ package com.paymybuddy.paymybuddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class PayMyBuddyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PayMyBuddyApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        SpringApplication.run(PayMyBuddyApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
 }
