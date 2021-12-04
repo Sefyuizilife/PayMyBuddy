5bb7782cd65fc9281e2a0433896c04cf0b0e9f2c
Sabouni Youssef <youssefsabouni@icloud.com>
2021-12-04 11:31:34 +0100

    feature(data script): add an additional user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,6 +5,8 @@ VALUES (1, 'FR30001019010000Z67067031', 'alex@email.valid', 'Alex', 'Xela', 50,
        (2, 'FR30001019010000Z67067032', 'nico@email.valid', 'Nicolas', 'Ocin', 10000,
         '$2a$10$cfNmqMhnzU1/FztEeodiqeQbmp3ArajI/kAt6KNXgzgPh0xXQ39Qi', now(), now()),
        (3, 'FR30001019010000Z67067033', 'mika@email.valid', 'Mickael', 'Akcim', 5000,
+        '$2a$10$cfNmqMhnzU1/FztEeodiqeQbmp3ArajI/kAt6KNXgzgPh0xXQ39Qi', now(), now()),
+       (4, 'FR30001019010000Z67067033', 'benoit@email.valid', 'Benoit', 'Tioneb', 5000,
         '$2a$10$cfNmqMhnzU1/FztEeodiqeQbmp3ArajI/kAt6KNXgzgPh0xXQ39Qi', now(), now());
 
 INSERT INTO `pay_my_buddy`.`user_contacts` (user_id, contacts_id)
