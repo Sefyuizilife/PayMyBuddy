95e9108e68e1c73a47d48e6eb9028899dd6e9dca
Sabouni Youssef <youssefsabouni@icloud.com>
2021-12-29 10:33:19 +0100

    Add the javadoc

--- src/main/java/com/paymybuddy/paymybuddy/configurations/WebSecurityConfiguration.java
+++ src/main/java/com/paymybuddy/paymybuddy/configurations/WebSecurityConfiguration.java
@@ -8,19 +8,43 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * This class extends the {@link WebSecurityConfigurerAdapter} interface to customise the security rules automatically
+ * defined by Spring Security with the {@link EnableWebSecurity} annotation.
+ */
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final UserService           userService;
     private final BCryptPasswordEncoder encoder;
 
+    /**
+     * Create a new instance of {@link WebSecurityConfiguration}. This will be done automatically by SpringBoot with
+     * dependency injection.
+     *
+     * @param userService
+     *         instance of {@link UserService}.
+     * @param encoder
+     *         instance of {@link BCryptPasswordEncoder} that uses the BCrypt strong hashing function implementing
+     *         {@link PasswordEncoder}.
+     */
     public WebSecurityConfiguration(UserService userService, BCryptPasswordEncoder encoder) {
 
         this.userService = userService;
         this.encoder     = encoder;
     }
 
+    /**
+     * Any endpoint that requires defense against common vulnerabilities can be specified here, including public ones.
+     *
+     * @param http
+     *         the {@link HttpSecurity} to modify
+     *
+     * @throws Exception
+     *         if an error occurs.
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
@@ -36,12 +60,28 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.logout().logoutSuccessUrl("/login");
     }
 
+    /**
+     * Allows you to specify the {@link AuthenticationManager}.
+     *
+     * @param authManagerBuilder
+     *         allows for easily building in memory authentication
+     *
+     * @throws Exception if an error occurs.
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
 
         authManagerBuilder.userDetailsService(userService).passwordEncoder(encoder);
     }
 
+    /**
+     * Expose le {@link AuthenticationManager} en tant que Bean.
+     *
+     * @return the {@link AuthenticationManager}.
+     *
+     * @throws Exception
+     *         if an error occurs.
+     */
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
--- src/main/java/com/paymybuddy/paymybuddy/controllers/UserController.java
+++ src/main/java/com/paymybuddy/paymybuddy/controllers/UserController.java
@@ -3,22 +3,45 @@ package com.paymybuddy.paymybuddy.controllers;
 import com.paymybuddy.paymybuddy.entities.User;
 import com.paymybuddy.paymybuddy.services.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
 
+/**
+ * Controller specifically for managing the {@link User}.
+ */
 @Controller
 public class UserController {
 
     private final UserService userService;
 
+    /**
+     * /** Create a new instance of this {@link TransactionController}. This will be done automatically by SpringBoot
+     * with dependencies injection.
+     *
+     * @param userService
+     *         an instance of {@link UserService}.
+     */
     UserController(UserService userService) {
 
         this.userService = userService;
     }
 
+    /**
+     * Allows you to associate a contact with the logged-in user.
+     *
+     * @param principal
+     *         represents the logged-in user.
+     * @param email
+     *         represents the contact to be associated.
+     * @param redirectAttributes
+     *         is linked to the {@link Model} interface and adds attributes for the redirection.
+     *
+     * @return a {@link String} that refers to the name of a view.
+     */
     @PostMapping("connect")
     public String connect(Principal principal, String email, RedirectAttributes redirectAttributes) {
 
@@ -36,6 +59,11 @@ public class UserController {
         return "redirect:transactions";
     }
 
+    /**
+     * Shows the login page
+     *
+     * @return a {@link String} that refers to the name of a view.
+     */
     @GetMapping("/login")
     public String login() {
 
