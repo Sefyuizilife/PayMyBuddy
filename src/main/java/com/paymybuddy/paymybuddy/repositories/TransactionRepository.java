af7b002e174eefa9c8e88431fc24905eff1c8d67
Sabouni Youssef <youssefsabouni@icloud.com>
2022-01-04 10:53:40 +0100

    Add the javadoc

--- src/main/java/com/paymybuddy/paymybuddy/repositories/TransactionRepository.java
+++ src/main/java/com/paymybuddy/paymybuddy/repositories/TransactionRepository.java
@@ -6,7 +6,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+/**
+ * Data Access Layer for the {@link Transaction}. It's managed here by the JPA interface.
+ */
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
+    /**
+     * Finds all transactions related to a donor or beneficiary.
+     *
+     * @param donor
+     *         is the transmitter of the {@link Transaction}.
+     * @param beneficiary
+     *         is the recipient of {@link Transaction}.
+     *
+     * @return {@link List} of {@link Transaction}.
+     */
     List<Transaction> findAllByDonorOrBeneficiary(User donor, User beneficiary);
 }
